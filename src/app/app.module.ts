94065d155d75c1ca9ec1ebe6c2550652cbb6959a
Andrzej Bronisławski <eazymen789@gmail.com>
2021-09-01 21:52:16 +0200

    Added @angular/material package and implemented the module holding all needed imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { MaterialModule } from './core/material/material.module';
 
 @NgModule({
 	declarations: [
@@ -10,12 +13,15 @@ import { AppComponent } from './app.component';
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
-	],
-	providers: [
+		BrowserAnimationsModule,
+		MaterialModule,
+		AppRoutingModule,
+		HttpClientModule
 
 	],
-	bootstrap: [AppComponent]
+	bootstrap: [
+		AppComponent
+	]
 })
 
 export class AppModule { }
